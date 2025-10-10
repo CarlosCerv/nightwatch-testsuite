d972272a4db50f3aec70720edc82f33989384f0d
Carlos Eduardo Cervantes Arteaga <carlos-cerv@outlook.es>
2025-10-10 14:37:24 -0600

    Fix several issues in GitHub actions

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -25,43 +25,56 @@ module.exports = {
     chrome: {
       webdriver: {
         start_process: true,
-        server_path: process.env.CHROMEDRIVER_PATH || (() => {
-          try {
-            return require('chromedriver').path;
-          } catch (e) {
-            console.error('ChromeDriver not found:', e.message);
-            return null;
-          }
-        })(),
+        server_path: process.env.CHROMEDRIVER_PATH || require('chromedriver').path,
         port: 9515,
-        cli_args: ['--verbose']
+        cli_args: [
+          '--verbose'
+        ]
       },
       desiredCapabilities: {
         browserName: 'chrome',
         'goog:chromeOptions': {
           args: [
             '--no-sandbox',
-            '--headless',
+            '--headless=new',
             '--disable-gpu',
             '--disable-dev-shm-usage',
-            '--disable-software-rasterizer'
+            '--disable-software-rasterizer',
+            '--window-size=1920,1080'
           ],
           w3c: true
-        }
+        },
+        acceptInsecureCerts: true
+      },
+      globals: {
+        waitForConditionTimeout: 10000,
+        retryAssertionTimeout: 5000
       }
     },
     
     safari: {
       webdriver: {
         start_process: true,
-        server_path: '/usr/bin/safaridriver',
-        port: 4444
+        server_path: process.env.SAFARI_DRIVER_PATH || '/usr/bin/safaridriver',
+        port: 4444,
+        host: 'localhost',
+        cli_args: [
+          '--port', '4444',
+          '--diagnostic-logging'
+        ]
       },
       desiredCapabilities: {
         browserName: 'safari',
         'safari:options': {
-          automaticInspection: false
-        }
+          automaticInspection: false,
+          automaticProfiling: false,
+          acceptInsecureCerts: true
+        },
+        acceptSslCerts: true
+      },
+      globals: {
+        waitForConditionTimeout: 10000,
+        retryAssertionTimeout: 5000
       }
     },
     
